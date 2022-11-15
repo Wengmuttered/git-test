2a010d719a9a63a8f1497fc6dbb41654bc92719c
Wengmuttered <1967748190@qq.com>
2022-11-15 10:32:00 +0800

    push commit3
    Signed-off-by: Wengmuttered <1967748190@qq.com>

--- src/main/java/com/tiwjk/git/GitTest.java
+++ src/main/java/com/tiwjk/git/GitTest.java
@@ -7,10 +7,8 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hello git4!!!!!");
-
-
         System.out.println("hello git4444!");
-
+        System.out.println("phsu git555!");
 
 
     }
