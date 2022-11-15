5178816c9d8b2254442d6d2f52487b94a3a1a9c9
分你一颗糖 <114347808+Wengmuttered@users.noreply.github.com>
2022-11-15 16:30:40 +0800

    pull commit

--- src/main/java/com/tiwjk/git/GitTest.java
+++ src/main/java/com/tiwjk/git/GitTest.java
@@ -15,7 +15,7 @@ public class GitTest {
         System.out.println("hello git4!!!!!");
         System.out.println("hello git4!!!!!");
         System.out.println("hello git4!!!!!");
-
+        System.out.println("pull pushgit");
 
 
 
